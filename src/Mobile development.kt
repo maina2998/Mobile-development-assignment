6f5a70382fefe14ee4bc350b2af2d8de44dd2135
student <mainagracewacheke@gmail.com>
2021-02-20 22:11:28 +0300

    first mobile development assignment

---
+++ src/Mobile development.kt
@@ -0,0 +1,13 @@
+fun main() {
+    val fullName = "Grace Wacheke Maina"
+    var age = 21
+    var phoneNumber = "0797854442"
+    var weight = 56
+    val citizen = "Kenyan"
+    println(fullName)
+    println(age)
+    println(phoneNumber)
+    println(weight)
+    println(citizen)
+
+}
